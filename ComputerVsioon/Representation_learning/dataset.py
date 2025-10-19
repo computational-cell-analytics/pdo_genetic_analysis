8ef60e66def34ca44b539cb8b18302c10aa24e1c
sina34300 <134196234+sina34300@users.noreply.github.com>
2025-10-19 13:26:30 +0200

    Update dataset.py

--- ComputerVsioon/Representation_learning/dataset.py
+++ ComputerVsioon/Representation_learning/dataset.py
@@ -25,6 +25,79 @@ import utils
 
 print(torch.cuda.is_available())
 
+def custom_collate_fn(batch):
+    # 'batch' is a list of tuples like [(rescaled_img1, non_rescaled_img1), (rescaled_img2, non_rescaled_img2), ...]
+    # We want to stack only the first element of each tuple.
+    rescaled_images = torch.stack([item[1] for item in batch])
+    labels = torch.LongTensor([item[3] for item in batch])
+    exp_days = torch.LongTensor([item[4] for item in batch])
+    pat_names = [item[5] for item in batch]
+    file_names = [item[6] for item in batch]
+    # print(len(rescaled_images))
+    return [rescaled_images, labels, exp_days, pat_names, file_names]
+
+class CellDataset_longitude(Dataset):
+    def __init__(self, filtered_csv, width_threshold, heigth_threshold, cell_label, transform=None):
+        self.transform = transform
+        self.filetered_metadata = filtered_csv
+        self.cell_data = []  # list of (cell_image, optional_info)
+        self.morphs_data = []
+        self.pat_names = []
+        self.exp_days = []
+        self.file_names = []
+
+        for index, row in self.filetered_metadata.iterrows():
+            device = row['device']
+            vessel_id = row['vessel_id']
+            file_name = row['file_name']
+            time_step = row['time_step']
+            pat_name = row['patient_name']
+            exp_day = int(float(row['exp_day']))
+            try:
+                    mask_path = f"/scratch-grete/projects/nim00007/data/pdo/data/pdo/segmentations/{device}/vessel_{vessel_id}/{file_name.split('.')[0]}.h5"
+                    img_path = f"/scratch-grete/projects/nim00007/data/pdo/data/pdo/images/{device}/vessel_{vessel_id}/{file_name.split('.')[0]}.h5"
+                    with h5py.File(mask_path, "r") as f:
+                        # print(f.keys())
+                        g = f['cellpose/v1']
+                        last_tp = list(g.keys())
+                        mask = g[time_step][:]
+                    with h5py.File(img_path, 'r') as f:
+                        g = f['timepoints']
+                        last_tp = list(g.keys())
+                        img = g[time_step][:]
+                        #print(last_tp)
+                    cells, morphs = extract_cells_from_image(img, mask, width_threshold, heigth_threshold, cell_label, exp_day)
+                    self.pat_names += [pat_name] * len(cells)
+                    self.cell_data.extend(cells)
+                    self.morphs_data.extend(morphs)
+                    self.exp_days += [exp_day] * len(cells)
+                    self.file_names += [file_name] * len(cells)
+            except Exception as error:
+                    print(error)
+                    continue
+            
+        self.cell_label = cell_label
+
+    def __len__(self):
+        return len(self.cell_data)
+
+    def __getitem__(self, idx):
+        cell = self.cell_data[idx]
+        cell_label = self.cell_label
+        morph = self.morphs_data[idx]
+        pat_name = self.pat_names[idx]
+        exp_day = self.exp_days[idx]
+        file_name = self.file_names[idx]
+
+        if self.transform:
+            transformed_cell = self.transform(cell)
+            cell = torch.from_numpy(cell)
+        else:
+            cell = torch.from_numpy(cell)
+            transformed_cell = cell
+
+        return cell, transformed_cell, morph, cell_label, exp_day, pat_name, file_name
+
 def circles(image):
     cv2.HoughCircles(image, 
             cv2.HOUGH_GRADIENT, 
@@ -95,6 +168,31 @@ class rescale_pad(object):
         return padded
     
 
+class rescale_pad_no_pad(object):
+    def __init__(self, target_w, target_h):
+        self.target_w = target_w
+        self.target_h = target_h
+        self.sharpen_kernel = np.array([[ 0, -1,  0],
+                           [-1,  5, -1],
+                           [ 0, -1,  0]])
+    
+    def apply_clahe(gray_img, clip_limit=2.0, tile_grid_size=(8, 8)):
+    # Make sure the input is uint8
+        if gray_img.dtype != np.uint8:
+            gray_img = cv2.normalize(gray_img, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+            clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
+        return clahe.apply(gray_img)
+    
+    def __call__(self, sample):
+        # sample = apply_clahe(sample)
+        # sample = cv2.filter2D(sample, -1, self.sharpen_kernel)
+        h, w = sample.shape[:2]
+        scale = min(self.target_h/h, self.target_w/w)
+        resized = cv2.resize(sample, (self.target_w, self.target_h), interpolation=cv2.INTER_LINEAR if scale < 1 else cv2.INTER_CUBIC)
+        return resized
+
+
+
 composed_rescale_augment = transforms.Compose([rescale_pad(64, 64),
                                transforms.ToPILImage(),  # Needed for torchvision transforms below
                                 transforms.RandomHorizontalFlip(),
@@ -105,7 +203,7 @@ composed_rescale_augment = transforms.Compose([rescale_pad(64, 64),
                                 transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 1.0)),
                                transforms.ToTensor()])
 
-composed_rescale = transforms.Compose([rescale_pad(64, 64),
+composed_rescale = transforms.Compose([rescale_pad_no_pad(64, 64),
                                transforms.ToTensor()])
 
 
